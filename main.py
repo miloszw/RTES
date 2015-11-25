54ce65270a123563b5edb32f53b264b894a5c2bb
Milosz <miloszw@kth.se>
2015-11-24 20:43:01 -0600

    Boilerplate code

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from router import Router
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except (IndexError, ValueError):
+        print("Usage python3 main.py <PORT>")
+        exit(1)
+
+    router = Router()
+
+    print(router.version)
---
+++ router.py
@@ -0,0 +1,3 @@
+class Router:
+
+    version = 0.1
